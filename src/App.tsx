284c91445637b81759286d88a3343c534e162a30
Jaroslav Zhanel <zhanel@tech4bet.com>
2021-11-15 00:16:05 +0100

    fetch data from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import React, {useState} from 'react';
-import './App.css';
-import RecordList from "./components/RecordList";
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
-import data from './data/task.recording.json';
 import {EventRecord} from "./types/EventRecord";
+import {SERVER_URL} from "./config";
+import RecordList from "./components/RecordList";
+import Message from "./components/Message";
 
 function App() {
 
-    const [records, setRecords] = useState<EventRecord[]>(data.records || []);
+    const [records, setRecords] = useState<EventRecord[] | null>(null);
+    const [messageOpen, setMessageOpen] = useState(false);
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+
+        async function fetchData() {
+
+            try {
+                const res = await axios.get(SERVER_URL);
+                setRecords(res.data.records as EventRecord[])
+            } catch (e) {
+                console.log(e);
+                setMessage("Couldn't fetch data from server");
+                setMessageOpen(true);
+            }
+
+        }
+
+        fetchData();
+
+    }, [])
 
     const updateRecords = (records: EventRecord[]) => {
         setRecords([...records])
@@ -15,7 +39,15 @@ function App() {
 
     return (
         <div className="App">
-            <RecordList records={records} updateRecords={updateRecords}></RecordList>
+            <Message open={messageOpen} message={message} handleClose={() => setMessageOpen(false)} severity="error" />
+            {!records &&
+                <Box sx={{ flexGrow: 1, display: 'flex' }}>
+                    <CircularProgress style={{margin: 'auto'}} />
+                </Box>
+            }
+            {records &&
+                <RecordList records={records || []} updateRecords={updateRecords} />
+            }
         </div>
     );
 }
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,22 +1,24 @@
 import Snackbar from "@mui/material/Snackbar";
-import Alert from "@mui/material/Alert";
+import Alert, {AlertColor} from "@mui/material/Alert";
 import React from "react";
 
 interface AppProps {
     open: boolean
     message: string,
     handleClose: () => void
+    severity?: AlertColor
 }
 
-function Message ({ open, message, handleClose }: AppProps) {
+function Message ({ open, message, handleClose, severity }: AppProps) {
     return (
         <>
             <Snackbar
                 open={open}
                 autoHideDuration={6000}
                 onClose={handleClose}
+                anchorOrigin={{vertical: "top", horizontal: "center"}}
             >
-                <Alert severity="success">{message}</Alert>
+                <Alert severity={severity || "success"}>{message}</Alert>
             </Snackbar>
 
         </>
