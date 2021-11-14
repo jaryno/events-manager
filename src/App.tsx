856149009012399b3563d743a2e26c181ebed5ed
Jaroslav Zhanel <zhanel@tech4bet.com>
2021-11-14 12:19:49 +0100

    drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import RecordList from "./components/RecordList";
 
 import data from './data/task.recording.json';
+import {Record} from "./types/Record";
 
 function App() {
-  return (
-    <div className="App">
-      <RecordList records={data.records || []}></RecordList>
-    </div>
-  );
+
+    const [records, setRecords] = useState<Record[]>(data.records || []);
+
+    const updateRecords = (records: Record[]) => {
+        setRecords([...records])
+    }
+
+    return (
+        <div className="App">
+            <RecordList records={records} updateRecords={updateRecords}></RecordList>
+        </div>
+    );
 }
 
 export default App;
--- src/components/RecordList.tsx
+++ src/components/RecordList.tsx
@@ -1,4 +1,4 @@
-import {Record} from "../types/Record";
+import React from 'react';
 
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
@@ -6,21 +6,54 @@ import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import {TableBody} from "@mui/material";
 import {formatTimestamp} from "../utils/time";
+import {
+    DragDropContext,
+    Draggable,
+    Droppable,
+    DropResult
+} from 'react-beautiful-dnd';
 
+import {Record} from "../types/Record";
 
 interface AppProps {
-    records: Record[]
+    records: Record[],
+    updateRecords: (records: Record[]) => void
 }
 
-const renderRows = (records: Record[]) => records.map((record, index) =>
-    <TableRow key={index}>
-        <TableCell>{record.event?.type || ""}</TableCell>
-        <TableCell>{record.setup?.nodeName || ""}</TableCell>
-        <TableCell>{formatTimestamp(record.time || 0)}</TableCell>
-    </TableRow>
+const renderRows = (records: Record[]) =>
+    records.map((record, i) => {
+        return <Draggable
+            draggableId={`${i}`}
+            index={i}
+            key={i}
+        >
+            {(provided, snapshot) => (
+                <TableRow
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                >
+                    <TableCell>{record.event?.type || ""}</TableCell>
+                    <TableCell>{record.setup?.nodeName || ""}</TableCell>
+                    <TableCell>{formatTimestamp(record.time || 0)}</TableCell>
+                </TableRow>
+            )}
+        </Draggable>
+    }
 );
 
-function RecordList({ records } : AppProps) {
+function RecordList({ records, updateRecords } : AppProps) {
+
+    const onDragEnd = (result: DropResult) => {
+        if (!result.destination) {
+            return
+        }
+
+        const newRecords = records.slice(0);
+        const [removed] = newRecords.splice(result.source.index, 1);
+        newRecords.splice(result.destination.index, 0, removed);
+        updateRecords(newRecords);
+    }
 
     return (
         <div>
@@ -32,9 +65,16 @@ function RecordList({ records } : AppProps) {
                         <TableCell>Date Time</TableCell>
                     </TableRow>
                 </TableHead>
-                <TableBody>
-                    {renderRows(records)}
-                </TableBody>
+
+                <DragDropContext onDragEnd={onDragEnd}>
+                    <Droppable droppableId={'droppable-table'} direction="vertical">
+                        {(provided, snapshot) => (
+                            <TableBody ref={provided.innerRef} {...provided.droppableProps}>
+                                {renderRows(records)}
+                            </TableBody>
+                        )}
+                    </Droppable>
+                </DragDropContext>
             </Table>
         </div>
     )
